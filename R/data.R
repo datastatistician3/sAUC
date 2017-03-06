7cf5e877da5576fc826dfc295f9b80c904ca89c0
sbohora <som-bohora@ouhsc.edu>
2017-03-06 11:45:51 -0600

    finalizing sAUC

---
+++ R/data.R
@@ -0,0 +1,22 @@
+#' @title FASD data about effects of brochure types on alcohol use
+#'
+#' @description A dataset containing the alcohol use and other attributes.
+#'
+#' @docType data
+#'
+#' @name fasd
+#'
+#' @usage data(fasd)
+#'
+#' @format A data frame with 210 rows and 4 variables:
+#' \describe{
+#'   \item{y}{alcohol use, number of drinks}
+#'   \item{group}{types of brochures}
+#'   \item{x1}{Whether vitamin was taken or not}
+#'   \item{x2}{Smoking status}
+#' }
+#' @source \url{http://www.netfas.net/}
+#'
+#' @examples
+#' data(fasd)
+NULL
