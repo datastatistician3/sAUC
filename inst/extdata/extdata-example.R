640deba1fa2416ae79985073246420d0c19d765f
Som Bohora <som-bohora@ouhsc.edu>
2018-04-28 15:09:15 -0500

    change eg dir

--- inst/extdata/extdata-example.R
+++ inst/extdata/extdata-example.R
@@ -7,7 +7,7 @@ sAUC::sAUC(formula = y ~ x2, treatment_group = "group", data = fasd)
 sAUC::sAUC(formula = y ~ x1 + x2, treatment_group = "group", data = fasd)
 
 
-fasd_label <- read.csv("../extdata/fasd-labels.csv")
+fasd_label <- read.csv("./inst/extdata/fasd-labels.csv")
 fasd_label[, c("smoke", "vitamin", "group")] <- lapply(fasd_label[, c("smoke", "vitamin", "group")], function(x) factor(x))
 
 sAUC::sAUC(formula = y ~ smoke, treatment_group = "group", data = fasd_label)
